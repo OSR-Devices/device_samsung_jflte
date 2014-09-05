b890cd64e8a94281a7cda55ce7588bc069f2b8e4
superatmel <superatmel@gmail.com>
2014-09-06 01:15:44 +0200

    Prepare for OSR

---
+++ osr.mk
@@ -0,0 +1,13 @@
+$(call inherit-product, device/samsung/jflte/full_jflte.mk)
+
+# Enhanced NFC
+$(call inherit-product, vendor/osr/config/nfc_enhanced.mk)
+
+# Inherit some common slim stuff.
+$(call inherit-product, vendor/osr/config/common_full_phone.mk)
+
+# Inherit multicarrier.
+$(call inherit-product, vendor/osr/config/common_multicarrier.mk)
+
+PRODUCT_NAME := osr_jflte
+PRODUCT_DEVICE := jflte
